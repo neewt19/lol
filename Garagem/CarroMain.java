8ddd83da3cbe4fe9f18a0290f5ed2e6d2c5820d0
neewt19 <newthonaraujo19@gmail.com>
2023-05-04 10:41:14 -0300

    se pa finalizei

--- Garagem/CarroMain.java
+++ Garagem/CarroMain.java
@@ -11,7 +11,7 @@ public class CarroMain {
         Garagem garagem = new Garagem();
 
         ArrayList<CarroClass> carros = new ArrayList<>();
-        Pattern patternInt = Pattern.compile("[0-9]{0,10}");
+        Pattern patternInt = Pattern.compile("[0-9]{1,10}");
         Pattern patternDouble = Pattern.compile("[0-90.0-9.0]{1,10}");
         Pattern patternString = Pattern.compile("[a-zA-Z0-9 ]{1,10}");
         Pattern patternPlaca = Pattern.compile("[A-Z]{4}[0-9]{4}");
@@ -35,87 +35,93 @@ public class CarroMain {
                 case "1" -> {
                     System.out.println("Adicione um carro a sua garagem: ");
                     String marc = JOptionPane.showInputDialog("Diga a marca do seu carro");
-                    while (marc == null){
-                        System.out.println("Marca inválida");
+                    if (marc == null){
                         break;
                     }
                     while (!patternString.matcher(marc).matches()){
                         System.out.println("Marca inválida");
                         marc = JOptionPane.showInputDialog("Diga a marca do seu carro");
-                        while (marc == null){
-                            System.out.println("Marca inválida");
-                            marc = JOptionPane.showInputDialog("Diga a marca do seu carro");
+                        if (marc == null){
+                            break;
                         }
+                    }
+                    if (marc == null){
+                        break;
                     }
 
                     String mod = JOptionPane.showInputDialog("Diga o modelo");
-                    while (mod == null){
-                        System.out.println("Modelo inválido");
-                        mod = JOptionPane.showInputDialog("Diga o modelo");
+                    if (mod == null){
+                        break;
                     }
                     while (!patternString.matcher(mod).matches()){
                         System.out.println("Modelo inválido");
                         mod = JOptionPane.showInputDialog("Diga o modelo");
-                        while (mod == null){
-                            System.out.println("Modelo inválido");
-                            mod = JOptionPane.showInputDialog("Diga o modelo");
+                        if (mod == null){
+                            break;
                         }
+                    }
+                    if (mod == null){
+                        break;
                     }
 
                     String ano = JOptionPane.showInputDialog("Diga o ano:");
-                    while (ano == null){
-                        System.out.println("Ano inválido");
-                        ano = JOptionPane.showInputDialog("Diga o ano:");
+                    if (ano == null){
+                        break;
                     }
                     while (!patternAno.matcher(ano).matches()){
                         System.out.println("Ano inválido");
                         ano = JOptionPane.showInputDialog("Diga o ano:");
-                        while (ano == null){
-                            System.out.println("Ano inválido");
-                            ano = JOptionPane.showInputDialog("Diga o ano:");
+                        if (ano == null){
+                            break;
                         }
+                    }
+                    if (ano == null){
+                        break;
                     }
 
                     String placa = JOptionPane.showInputDialog("Qual a placa? ");
-                    while (placa == null){
-                        System.out.println("Placa inválida");
-                        placa = JOptionPane.showInputDialog("Qual a placa? ");
+                    if (placa == null){
+                        break;
                     }
                     while (!patternPlaca.matcher(placa).matches()){
                         System.out.println("Placa inválida");
                         placa = JOptionPane.showInputDialog("Qual a placa? ");
-                        while (placa == null){
-                            System.out.println("Placa inválida");
-                            placa = JOptionPane.showInputDialog("Qual a placa? ");
+                        if (placa == null){
+                            break;
                         }
+                    }
+                    if (placa == null){
+                        break;
                     }
 
                     String combustivel = JOptionPane.showInputDialog("Quantos litros de combustível tem no carro?");
-                    while (combustivel == null){
-                        System.out.println("Combustível inválido");
-                        combustivel = JOptionPane.showInputDialog("Quantos litros de combustível tem no carro?");
+                    if (combustivel == null){
+                        break;
                     }
                     while (!patternDouble.matcher(combustivel).matches()){
                         System.out.println("Combustível inválido");
                         combustivel = JOptionPane.showInputDialog("Quantos litros de combustível tem no carro?");
-                        while (combustivel == null){
-                            System.out.println("Combustível inválido");
-                            combustivel = JOptionPane.showInputDialog("Quantos litros de combustível tem no carro?");
+                        if (combustivel == null){
+                            break;
                         }
+                    }
+                    if (combustivel == null){
+                        break;
                     }
 
                     String km = JOptionPane.showInputDialog("Quantos km rodados?");
-                    while (km == null){
-                        System.out.println("Km inválido");
-                        km = JOptionPane.showInputDialog("Quantos km rodados?");
+                    if (km == null){
+                        break;
                     }
                     while (!patternDouble.matcher(km).matches()){
                         System.out.println("Km inválido");
                         km = JOptionPane.showInputDialog("Quantos km rodados?");
-                        while (km == null){
-                            System.out.println("Km inválido");
-                            km = JOptionPane.showInputDialog("Quantos km rodados?");
+                        if (km == null){
+                            break;
                         }
+                    }
+                    if (km == null){
+                        break;
                     }
 
                     CarroClass novoCarro = new CarroClass();
@@ -135,31 +141,28 @@ public class CarroMain {
                             System.out.printf("carro[%d] %s %s\n",num, carros.get(i).getMarca(),carros.get(i).getModelo());
                         }
                         boolean loop = false;
-                        while (!loop){
-                            String escolha = JOptionPane.showInputDialog("Escolha um carro para remover");
-                            while (escolha == null){
-                                System.out.println("Carro inválido");
-                                escolha = JOptionPane.showInputDialog("Escolha um carro para remover");
-                            }
-                            int escolhaTest = Integer.parseInt(escolha);
-                            while (!patternInt.matcher(escolha).matches()){
-                                System.out.println("isso não é um numero");
-                                escolha = JOptionPane.showInputDialog("Escolha um carro para remover");
-                                while (escolha == null){
-                                    System.out.println("Carro inválido");
-                                    escolha = JOptionPane.showInputDialog("Escolha um carro para remover");
-                                }
-                            }
-                            if (carros.size() >= escolhaTest){
-                                escolhaTest = escolhaTest -1;
-                                System.out.println("carro removido com sucesso");
-                                carros.remove(escolhaTest);
-                                loop = true;
+                        String escolha = JOptionPane.showInputDialog("Escolha um carro para remover");
+                        if (escolha == null){
+                            break;
+                        }
+                        if (!patternInt.matcher(escolha).matches()){
+                            System.out.println("isso não é um numero");
+                            break;
+                        }
+                        if (escolha == null){
+                            break;
+                        }
+                        int escolhaTest = Integer.parseInt(escolha);
+
+                        if (carros.size() >= escolhaTest){
+                            escolhaTest = escolhaTest -1;
+                            System.out.println("carro removido com sucesso");
+                            carros.remove(escolhaTest);
+                            loop = true;
                             }else {
                                 System.out.println("Carro não existe");
                                 loop = true;
                             }
-                        }
                     } else {
                         System.out.println("Não há carros na garagem.");
                     }
@@ -189,17 +192,18 @@ public class CarroMain {
                         }
 
                         String escolha =JOptionPane.showInputDialog("Escolha um carro para andar");
-                        while (escolha == null){
-                            System.out.println("Carro inválido");
-                            escolha =JOptionPane.showInputDialog("Escolha um carro para andar");
+                        if (escolha == null){
+                            break;
                         }
                         while (!patternInt.matcher(escolha).matches()){
                             System.out.println("Carro inválido");
                             escolha =JOptionPane.showInputDialog("Escolha um carro para andar");
-                            while (escolha == null){
-                                System.out.println("Carro inválido");
-                                escolha =JOptionPane.showInputDialog("Escolha um carro para andar");
+                            if (escolha == null){
+                                break;
                             }
+                        }
+                        if (escolha == null){
+                            break;
                         }
 
                         int escolha1 = Integer.parseInt(escolha)-1;
@@ -209,19 +213,19 @@ public class CarroMain {
                         if (num <= carros.size()) {
                             String kmfin2 = JOptionPane.showInputDialog("Quantos km vc vai rodar?");
 
-                            while (kmfin2 == null){
-                                System.out.println("Km inválido");
-                                kmfin2 = JOptionPane.showInputDialog("Quantos km vc vai rodar?");
+                            if (kmfin2 == null){
+                                break;
                             }
                             while (!patternDouble.matcher(kmfin2).matches()){
                                 System.out.println("Km inválido");
                                 kmfin2 = JOptionPane.showInputDialog("Quantos km vc vai rodar?");
-                                while (kmfin2 == null){
-                                    System.out.println("Km inválido");
-                                    kmfin2 = JOptionPane.showInputDialog("Quantos km vc vai rodar?");
+                                if (kmfin2 == null){
+                                    break;
                                 }
                             }
-
+                            if (kmfin2 == null){
+                                break;
+                            }
                             double kmfin = Double.parseDouble(kmfin2);
                             double kmfin1 = kmfin;
 
@@ -267,16 +271,14 @@ public class CarroMain {
                         }
 
                         String escolha = JOptionPane.showInputDialog("Escolha um carro para abastecer");
-                        while (escolha == null){
-                            System.out.println("Carro inválido");
-                            escolha = JOptionPane.showInputDialog("Escolha um carro para abastecer");
+                        if (escolha == null){
+                            break;
                         }
                         while (!patternInt.matcher(escolha).matches()){
                             System.out.println("Carro inválido");
                             escolha = JOptionPane.showInputDialog("Escolha um carro para abastecer");
-                            while (escolha == null){
-                                System.out.println("Carro inválido");
-                                escolha = JOptionPane.showInputDialog("Escolha um carro para abastecer");
+                            if (escolha == null){
+                                break;
                             }
                         }
 
@@ -284,17 +286,18 @@ public class CarroMain {
                         int num = Integer.parseInt(escolha);
                         if (num <= carros.size()) {
                             String abastecer = JOptionPane.showInputDialog("Quantos litros vai colocar?");
-                            while (abastecer == null){
-                                System.out.println("combustivel inválido");
-                                abastecer = JOptionPane.showInputDialog("Quantos litros vai colocar?");
+                            if (abastecer == null){
+                                break;
                             }
                             while (!patternDouble.matcher(abastecer).matches()){
                                 System.out.println("combustivel inválido");
                                 abastecer = JOptionPane.showInputDialog("Quantos litros de combustível tem no carro?");
-                                while (abastecer == null){
-                                    System.out.println("combustivel inválido");
-                                    abastecer = JOptionPane.showInputDialog("Quantos litros vai colocar?");
+                                if (abastecer == null){
+                                    break;
                                 }
+                            }
+                            if (abastecer == null){
+                                break;
                             }
                             double abs1 = Double.parseDouble(abastecer);
                             carros.get(escolha1).abastecer(abs1);
